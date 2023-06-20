6f094e66e1ad28594640c05a51b53491bf77cf2a
Caleb Serafin <calebserafin@outlook.com>
2023-06-20 20:23:52 +0200

    Made code example more descriptive.

--- SolveCheckerBoardWithKeyProblem/Program.cs
+++ SolveCheckerBoardWithKeyProblem/Program.cs
@@ -14,101 +14,118 @@
 /// <param name="boardSize">The size of the board.</param>
 /// <param name="board">The board.</param>
 /// <param name="coin">The coin.</param>
-void SolveBoard(int boardSize, long board, long coin)
-{
-    board = Math.Abs(board);
-    coin = Math.Abs(coin);
-    if (coin == 0) {
+void SolveBoard(byte boardSize, ulong headsOnBoard, ulong keyOnBoard) {
+    if (keyOnBoard == 0) {
         Console.WriteLine("There is not coin.");
         return;
     }
-    if (Math.Log2(board) > boardSize) {
+    if (ulong.Log2(headsOnBoard) > boardSize) {
         Console.WriteLine("Board is too big.");
         return;
     }
-    if (Math.Log2(coin) > boardSize) {
+    if (ulong.Log2(keyOnBoard) > boardSize) {
         Console.WriteLine("Coin is too big.");
         return;
     }
 
-    void PrintBin(long n) {
-        Console.Write(Convert.ToString(n, 2).PadLeft(boardSize, '0'));
+    void PrintBin(ulong n) {
+        Console.Write(Convert.ToString((long)n, 2).PadLeft(boardSize, '0'));
         Console.WriteLine($"    ={n}");
     }
-    Console.WriteLine("Board and Coin:");
-    PrintBin(board);
-    PrintBin(coin);
+    void PrintBinByte(byte n) {
+        byte log2OfBoardSize = byte.Log2(boardSize);
+        Console.Write(Convert.ToString(n, 2).PadLeft(log2OfBoardSize, '0').PadLeft(boardSize));
+        Console.WriteLine($"    ={n}");
+    }
+    Console.WriteLine("Chess Board. 0 represents tails, 1 represents heads:");
+    PrintBin(headsOnBoard);
+    Console.WriteLine("Key. 1 represents where the key is:");
+    PrintBin(keyOnBoard);
     Console.WriteLine("\n");
 
-    Console.WriteLine("XOR indices of 1s in board:");
-    long e = 0;
-    for (int i = 0; i < 63; ++i)
-    {
-        long boardShifted = board >> i;
-        if ((boardShifted & 0b1) == 1)
-        {
-            e ^= i;
-            PrintBin(i);
+    Console.WriteLine("XOR the encoded positions of all the heads on the board:");
+    IEnumerable<byte> EncodePositions(ulong board) {
+        for (byte i = 0; i < 63; ++i) {
+            // Check if bit is set
+            ulong singleSetBitMask = 1UL << i;
+            bool isBitSet = (board & singleSetBitMask) != 0;
+            if (isBitSet)
+                yield return i;
         }
+    }
+    byte EncodePosition(ulong board) {
+        return (byte)ulong.Log2(board);
+    }
+    ulong DecodePosition(byte position) {
+        return 1UL << position;
+    }
+    byte combinedHeadPositions = 0;
+    foreach (byte position in EncodePositions(headsOnBoard)) {
+        combinedHeadPositions ^= position;
+        PrintBinByte(position);
     }
     Console.WriteLine("^");
     Console.WriteLine(new string('-', boardSize));
-    PrintBin(e);
+    PrintBin(combinedHeadPositions);
     Console.WriteLine("\n");
 
 
-    Console.WriteLine("Log2 of key to get key index:");
-    PrintBin(coin);
-
-    long keyIndex = (long)Math.Log2(coin);
+    Console.WriteLine("Encode the key as it's position:");
+    PrintBin(keyOnBoard);
+    byte keyIndex = EncodePosition(keyOnBoard);
     Console.WriteLine("log2");
     Console.WriteLine(new string('-', boardSize));
-    PrintBin(keyIndex);
+    PrintBinByte(keyIndex);
     Console.WriteLine("\n");
 
-    Console.WriteLine("XOR keyIndex with e to get which coin to get flipIndex:");
-    PrintBin(keyIndex);
-    PrintBin(e);
-    long flipIndex = keyIndex ^ e;
+    Console.WriteLine("XOR the key's position with combined positions of the head coins to get index of position of the coin to flip:");
+    PrintBinByte(keyIndex);
+    PrintBinByte(combinedHeadPositions);
+    byte flipIndex = (byte)(keyIndex ^ combinedHeadPositions);
     Console.WriteLine("^");
     Console.WriteLine(new string('-', boardSize));
-    PrintBin(flipIndex);
+    PrintBinByte(flipIndex);
     Console.WriteLine("\n");
 
-    Console.WriteLine("Shift 1 with flipIndex to get flipMask:");
-    PrintBin(1);
-    PrintBin(flipIndex);
-    long flipMask = 1 << (int)flipIndex;
-    Console.WriteLine("^");
+    Console.WriteLine("Get the coin which needs to flip:");
+    PrintBinByte(1);
+    PrintBinByte(flipIndex);
+    ulong flipMask = DecodePosition(flipIndex);
+    Console.WriteLine("<<");
     Console.WriteLine(new string('-', boardSize));
     PrintBin(flipMask);
     Console.WriteLine("\n");
 
-    Console.WriteLine("XOR board with flipMask to create the new board:");
-    long board2 = board ^ flipMask;
-    PrintBin(board);
+    Console.WriteLine("XOR the board with flipped coin to create the new board:");
+    ulong board2 = headsOnBoard ^ flipMask;
+    PrintBin(headsOnBoard);
     PrintBin(flipMask);
     Console.WriteLine("^");
     Console.WriteLine(new string('-', boardSize));
     PrintBin(board2);
     Console.WriteLine("\n");
+    Console.WriteLine("\n");
 
-    Console.WriteLine("XOR indices of 1s in modified board to get the index of the flipped coin:");
-    long coin2 = 0;
-    for (int i = 0; i < 63; ++i)
-    {
-        long boardShifted = board2 >> i;
-        if ((boardShifted & 0b1) == 1)
-        {
-            coin2 ^= i;
-            PrintBin(i);
-        }
+    Console.WriteLine("From the reader's perspective:");
+    Console.WriteLine("XOR positions of heads in modified board to get the combined position. This is also the position of the key:");
+    byte key2Position = 0;
+    foreach (byte position in EncodePositions(board2)) {
+        key2Position ^= position;
+        PrintBinByte(position);
     }
     Console.WriteLine("^");
     Console.WriteLine(new string('-', boardSize));
-    PrintBin(coin2);
+    PrintBinByte(key2Position);
     Console.WriteLine("\n");
 
+    Console.WriteLine("What means that if the key is drawn on the board, it will look like:");
+    PrintBinByte(1);
+    PrintBinByte(key2Position);
+    Console.WriteLine("<<");
+    Console.WriteLine(new string('-', boardSize));
+    ulong key2OnBaord = DecodePosition(key2Position);
+    PrintBin(key2OnBaord);
+    Console.WriteLine("\n");
 }
 
 SolveBoard(8, 0b00101011, 0b00001000);
